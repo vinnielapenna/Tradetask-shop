2136df5f537217e399963a0d807c546b8fad822a
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-07 18:20:32 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to TradeTaskJobs</h1>
-      <p>This is your new Next.js homepage.</p>
-    </div>
-  );
-}
-#comment 
--- pages/shop.js
+++
@@ -1,8 +0,0 @@
-export default function Shop() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Shop (Coming Soon)</h2>
-      <p>This page will display tools and gear available through affiliate links.</p>
-    </div>
-  );
-}
