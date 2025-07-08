18c2116a18912972f1ec59e7c78111f060698ef7
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-07 22:12:13 -0400

    Add files via upload

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "Test User", email: credentials.email };
+        if (credentials.password === "test123") {
+          return user;
+        }
+        return null;
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt"
+  },
+});
