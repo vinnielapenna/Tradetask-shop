e96bbc32c54347d7b60e3b8fa1e92c5ea4f7b152
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-07 22:11:09 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to TradeTaskJobs</h1>
-      <p>This is the homepage.</p>
-    </main>
-  );
-}
--- pages/jobs.js
+++
@@ -1,8 +0,0 @@
-export default function Jobs() {
-  return (
-    <main>
-      <h1>Job Board</h1>
-      <p>Available job listings will appear here.</p>
-    </main>
-  );
-}
