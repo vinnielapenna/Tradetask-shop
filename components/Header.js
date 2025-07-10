ae48f94cb2fae5f78a29a414bd4470699fdc500f
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-09 21:43:35 -0400

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+      <Link href="/" style={{ marginRight: 15 }}>Home</Link>
+      <Link href="/jobs" style={{ marginRight: 15 }}>Jobs</Link>
+      <Link href="/shop" style={{ marginRight: 15 }}>Shop</Link>
+      <Link href="/login">Login</Link>
+    </nav>
+  );
+}
