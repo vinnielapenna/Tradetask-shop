c2e0a19ad7a3c864b2c3b53b2d8b9ba64cc00eaa
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-12 15:43:36 -0400

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -4,31 +4,43 @@ import ProductCard from "../components/ProductCard";
 const products = [
   {
     id: 1,
-    name: "Heavy-Duty Work Gloves",
-    image: "/gloves.jpg",
-    link: "https://www.amazon.com/s?k=work+gloves",
-    price: "$12.99"
+    name: "Jukmo Tactical Belt",
+    image: "https://m.media-amazon.com/images/I/81cr3lwVSxL._AC_SY355_.jpg",
+    link: "https://amzn.to/4lUTUyu",
+    price: "$19.98",
+    category: "Belt"
   },
   {
     id: 2,
-    name: "Steel Toe Work Boots",
-    image: "/boots.jpg",
-    link: "https://www.amazon.com/s?k=steel+toe+boots",
-    price: "$59.99"
+    name: "Rexbeti Knee Pads",
+    image: "https://m.media-amazon.com/images/I/81gkjvd9YFL._AC_SX425_.jpg",
+    link: "https://amzn.to/40NoZM9",
+    price: "$24.99",
+    category: "Kneepads"
   },
   {
     id: 3,
-    name: "Cordless Drill Set",
-    image: "/drill.jpg",
-    link: "https://www.amazon.com/s?k=cordless+drill",
-    price: "$79.99"
+    name: "HAPPY NUTS Comfort Cream Deodorant",
+    image: "https://m.media-amazon.com/images/I/61cG+UEBCxL._SX522_.jpg",
+    link: "https://amzn.to/44J6J7X",
+    price: "$14.99",
+    category: "Deoderant"
   },
   {
     id: 4,
-    name: "HVAC Multimeter Tool",
-    image: "/multimeter.jpg",
-    link: "https://www.amazon.com/s?k=hvac+multimeter",
-    price: "$34.99"
+    name: "Dickies Men's Socks",
+    image: "https://m.media-amazon.com/images/I/91DfBeaGyPL._AC_SY741_.jpg",
+    link: "https://amzn.to/40KBojR",
+    price: "$11.10",
+    category: "Socks"
+  },
+  {
+    id: 5,
+    name: "Neck Fan, AI 6 Speed Powerful Portable Fan",
+    image: "https://m.media-amazon.com/images/I/71EUT9awcGL._AC_SX425_.jpg",
+    link: "https://amzn.to/4lMZ5QH",
+    price: "$29.99",
+    category: "Fan"
   }
 ];
 
