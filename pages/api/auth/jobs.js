b20a21e49ef07a4b0cc11b772cd01619e5721be2
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-12 17:54:43 -0400

    Delete pages/api/auth/jobs.js

--- pages/api/auth/jobs.js
+++
@@ -1,9 +0,0 @@
-import { jobs } from "../../lib/data";
-
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json(jobs);
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
-  }
-}
