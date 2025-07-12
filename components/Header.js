7171e4133dfc94a1a0d11935611cea86ffee6703
vinnielapenna <vinniemlapenna@gmail.com>
2025-07-12 16:06:44 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,8 @@ export default function Header() {
       <Link href="/jobs" style={{ marginRight: 15 }}>Jobs</Link>
       <Link href="/post-job" style={{ marginRight: 15 }}>Post Job</Link>
       <Link href="/shop" style={{ marginRight: 15 }}>Shop</Link>
+      <Link href="/dashboard" style={{ marginRight: 15 }}>Dashboard</Link>
+      <Link href="/leave-review" style={{ marginRight: 15 }}>Leave Review</Link>
       <Link href="/login">Login</Link>
     </nav>
   );
