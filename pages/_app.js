a06bdefe9d12dcc82f9158da994dc1ef73819781
vinnielapenna <vinniemlapenna@gmail.com>
2025-08-07 21:12:29 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import { SessionProvider } from "next-auth/react";
+import type { AppProps } from 'next/app';
+import Navbar from '../components/Navbar'; // Make sure this matches your folder structure
+import '../styles/globals.css'; // If you have global styles
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
   );
